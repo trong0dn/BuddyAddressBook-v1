441163e3504b2e213f7bbaf26dc51e681fa5c482
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-11-24 02:38:06 -0500

    Added test suite

--- src/AddressBookTest.java
+++ src/AddressBookTest.java
@@ -5,12 +5,17 @@ public class AddressBookTest {
     private AddressBook addressbook;
     private BuddyInfo firstBuddy;
     private BuddyInfo secondBuddy;
+    private BuddyInfo thirdBuddy;
 
     @Before
     public void setup() {
         addressbook = new AddressBook();
         firstBuddy = new BuddyInfo("b1", "a1", "p1");
         secondBuddy = new BuddyInfo("b2", "a2", "p2");
+        thirdBuddy = new BuddyInfo("b3", "a3", "p3");
+        addressbook.addBuddy(firstBuddy);
+        addressbook.addBuddy(secondBuddy);
+        addressbook.addBuddy(thirdBuddy);
     }
 
     @Test
